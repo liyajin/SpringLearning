a12acf8c96acdf5cb1f04a0a85717f7474ecf902
liyajin <csuliyajin@gmail.com>
2016-01-22 16:58:13 +0800

    add chapter02 knight springido

--- src/main/java/com/springinaction/chapter02/springido1/Instrumentalist.java
+++ src/main/java/com/springinaction/chapter02/springido1/Instrumentalist.java
@@ -1,9 +1,12 @@
 package com.springinaction.chapter02.springido1;
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
 /**
  * Created by liyajin on 16/1/20.
  */
-public class Instrumentalist implements Performer {
+public class Instrumentalist implements Performer,InitializingBean,DisposableBean {
 
     private String song;
     private Instrument instrument;
@@ -30,5 +33,13 @@ public class Instrumentalist implements Performer {
 
     public void cleanInstrument(){
         instrument.clean();
+    }
+
+    public void destroy() throws Exception {
+        instrument.clean();
+    }
+
+    public void afterPropertiesSet() throws Exception {
+        instrument.tune();
     }
 }
